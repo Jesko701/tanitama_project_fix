45e8987c6b7966b25902318e10423f0dcfcee4a5
Jesko701 <IRZHunterBlood@yahoo.com>
2023-06-12 21:05:18 +0700

    change the port too 8080

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ def classification():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port='8000', debug='True')
+    app.run(host="0.0.0.0", port='8080', debug='True')
 else:
     print("Tidak bisa menjalankan program ini")
